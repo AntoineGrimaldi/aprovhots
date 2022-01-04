5de56b7cfa7059d146e22730a78e030ada4968ed
Antoine Grimaldi <antoine.grimaldi@univ-amu.fr>
2022-01-04 12:31:39 +0100

    make easy use of path

--- notebook/dataset_creation.py
+++ notebook/dataset_creation.py
@@ -1,9 +1,7 @@
-import ast, glob
+import ast, glob, os, tonic
 import pandas as pd
 from tqdm import tqdm
 import numpy as np
-from tonic.dataset import Dataset
-import os
 
 def csv_load(path, name):
     '''load events in .csv files with the same format as in tonic. 
@@ -89,7 +87,7 @@ def build_aprovis_dataset(path, labelz, patch_size = None, sensor_size=None, max
                 save_as_patches(events, path, label, name_num, patch_size = patch_size, sensor_size=sensor_size, max_duration=max_duration, min_num_events=min_num_events, train_test_ratio=train_test_ratio, ordering = ordering)
     else: print(f'this dataset was already created, check at : \n {path}')
             
-class Synthetic_Dataset(Dataset):
+class Synthetic_Dataset(tonic.dataset.Dataset):
     '''creates a dataset from .csv files in a specific 'path'. Adapted for Synthetic dataset given by Sotiris
     '''
     classes = ["sea", "gro"]
@@ -97,8 +95,6 @@ class Synthetic_Dataset(Dataset):
     sensor_size = (128, 128, 2)
     dtype = np.dtype([("x", int), ("y", int), ("t", int), ("p", int)])
     ordering = dtype.names
-    #absolute path of the .csv files 
-    path = '/home/INT/grimaldi.a/Documents/projets/WP3/2021-12-06_simulator_data/'
 
     def __init__(self, save_to, train=True, patch_size=None, max_duration=None, transform=None, target_transform=None):
         super(Synthetic_Dataset, self).__init__(
@@ -163,7 +159,7 @@ def get_isi(events, ordering = 'xytp', verbose = False):
         print(f'Mean ISI for OFF events: {np.round(isipol[0].mean()*1e-3,1)} in ms \n')
     return isipol
 
-def get_info(path = '/home/INT/grimaldi.a/Documents/projets/WP3/2021-12-06_simulator_data/',
+def get_info(path,
             patch_size = (16,16),
             max_duration = None,
             labelz = ['sea','gro'],
@@ -176,7 +172,7 @@ def get_info(path = '/home/INT/grimaldi.a/Documents/projets/WP3/2021-12-06_simul
     nb_ground, nb_sea, isi_neg_sea, isi_neg_ground, isi_pos_sea, isi_pos_ground, dura_sea, dura_ground = [0]*8
     print('Number of samples:\n') 
     for train in [False,True]:
-        dataset = Synthetic_Dataset(save_to=path, train=train, patch_size=patch_size, max_duration=max_duration)
+        dataset = Synthetic_Dataset(save_to=path, train=train, patch_size=patch_size, max_duration=max_duration, transform=tonic.transforms.NumpyAsType(int))
         if train: print(f'training set: {len(dataset)}')
         else: print(f'testing set: {len(dataset)}')
         for i in range(len(dataset)):
